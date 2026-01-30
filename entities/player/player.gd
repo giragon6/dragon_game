a6b707e397a1803221c26ee20d24b3124e89ee1e
giragon6 <emmemcdonald66@gmail.com>
2026-01-30 18:04:58 -0500

    convert to platformer lol

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
-@export var speed = 400
-var flipped: bool = false
+@export var speed = Vector2(400,600)
+var flipped: bool = true
+var rot := 0
 var screen_size : Vector2
+const GRAVITY := 300
+const TERM_VEL := 600
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -10,25 +13,33 @@ func _ready():
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
-		flipped = false
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
+		velocity.x += speed.x
 		flipped = true
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed.x
+		flipped = false
+		
+	if Input.is_action_just_pressed("up"):
+		velocity.y = -10000
+		rot = 315
 
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play("walk")
 	else:
+		# if grounded: $AnimatedSprite2D.play("idle")
+		# $AnimatedSprite2D.play("idle")
 		$AnimatedSprite2D.play("idle")
-		
+	velocity.y += GRAVITY
+	rot += 1
+	if rot >= 360: rot = 0
+	if rot < 315 and rot > 45: rot = 45
+	$AnimatedSprite2D.rotation_degrees = rot
+	
 	if $AnimatedSprite2D.scale.x == 1 and flipped: 
 		$AnimatedSprite2D.scale.x = -1
 	elif $AnimatedSprite2D.scale.x == -1 and not flipped: 
 		$AnimatedSprite2D.scale.x = 1
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	print($AnimatedSprite2D.rotation_degrees)
--- scenes/windows/inventory/item.gd
+++
@@ -1,7 +0,0 @@
-class_name Item
-extends Node
-
-var remove_on_use : bool = false
-
-func press(): 
-	pass
