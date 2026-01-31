e07aa032a31d1e280fdda7dc30e581e08332e43f
giragon6 <emmemcdonald66@gmail.com>
2026-01-31 17:32:46 -0500

    make player stick to raindrops

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var speed = 100
 @export var jump_strength = 1000
@@ -7,24 +8,26 @@ extends CharacterBody2D
 var rot := 0
 var screen_size : Vector2
 var rotation_direction = 0
+var raindrop_in : Raindrop = null
 const TERM_VEL := 600
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func get_input():
-	rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * speed
 
 func _physics_process(delta: float) -> void:
-	get_input()
-	rotation += rotation_direction * rotation_speed * delta	
+	if not raindrop_in:
+		rotation_direction = Input.get_axis("left", "right")
+		velocity = transform.x * speed
+		rotation += rotation_direction * rotation_speed * delta	
+		if not is_on_floor():
+			velocity.y += gravity * delta
+	else:
+		velocity.x = 0
+		velocity.y = raindrop_in.linear_velocity.y
 		
 	if Input.is_action_just_pressed("up"):
 		velocity.y = -jump_strength
-
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play("walk")
@@ -32,3 +35,13 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("idle")
 	
 	move_and_slide()
+	raindrop_in = null
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print(collision)
+		var collider = collision.get_collider()
+		if collider is Raindrop:
+			raindrop_in = collider
+			# get sucked into the raindrop because of surface tension or something
+			position = raindrop_in.position
+			print(raindrop_in)
--- entities/raindrop/raindrop.gd
+++ entities/raindrop/raindrop.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+class_name Raindrop
 
 func init(spawn_pos: Vector2) -> void:
 	position = spawn_pos
